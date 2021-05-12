d035ba5bc4c516eb72c493dbf7cbd34abb7a9f16
Bhupendra Patel <bhupendra.bp57@gmail.com>
2021-05-12 23:18:14 +0530

    log in completed

---
+++ app/src/main/java/com/hackerkernel/sharedprefrence/DashboardActivity.java
@@ -0,0 +1,37 @@
+package com.hackerkernel.sharedprefrence;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    Button logout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        logout=findViewById(R.id.logout);
+
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FirebaseAuth.getInstance().signOut();
+                Intent intent=new Intent(DashboardActivity.this,loginActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        });
+    }
+
+}
